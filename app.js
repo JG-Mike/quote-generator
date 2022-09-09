f1eab35c51d4aff141587b4beb8208d0c0ad78f7
michael <michael@pop-os.localdomain>
2022-09-09 22:39:52 +0300

    currently the button is not working

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 const btn = document.getElementById("btn")
-const outPut = document.getElementById("output")
+// const outPut = document.getElementById("output")
+let quote = document.getElementById("word")
+let author = document.getElementById("name")
 
-document.addEventListener("DOMContentLoaded", function clickAction(){
+
 fetch('http://api.quotable.io/random')
     .then(response => response.json())
     .then(quote => getQuote(quote))
 
+     
  function getQuote(data){
-    let animeQuote = document.createElement("li")
-    animeQuote.className = "quotesGenerated"
-    animeQuote.innerHTML =`
-      <div class="quotesGenerated">
-                <h3> ${data.author}</h3>
-				<p>${data.content}</p>
+     quote.innerText = data.content
+    author.innerHTML = data.author
+    // let animeQuote = document.createElement("li")
+    // animeQuote.className = "quotesGenerated"
+    // animeQuote.innerHTML =`
+    //   <div class="quotesGenerated">
+    //             <h3> ${data.author}</h3>
+	// 			<p>${data.content}</p>
 			
 
-    </div>
-    `;
-    console.log(animeQuote)
-    outPut.appendChild(animeQuote);
-   
+    // </div>
+    // `;
+    // outPut.appendChild(animeQuote)
+    // console.log(data)
 }
 
- btn.addEventListener("click",  getQuote()) 
-})
 
-
-// document.addEventListener("DOMContentLoaded",result(getQuote())) 
+  btn.addEventListener("click", getQuote);
