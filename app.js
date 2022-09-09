90f73c608675f0b7b9fe2e79f50357f513573430
michael <michael@pop-os.localdomain>
2022-09-09 19:48:29 +0300

    build the ui

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-// fetch api
-const url = '//api.quotable.io/random'
+const btn = document.getElementById("btn")
+const outPut = document.getElementById("output")
 
-fetch(url)
-.then(Response => Response.json())
-.then(data => console.log(data))
+document.addEventListener("DOMContentLoaded", function clickAction(){
+fetch('http://api.quotable.io/random')
+    .then(response => response.json())
+    .then(quote => getQuote(quote))
+
+ function getQuote(data){
+    let animeQuote = document.createElement("li")
+    animeQuote.className = "quotesGenerated"
+    animeQuote.innerHTML =`
+      <div class="quotesGenerated">
+                <h3> ${data.author}</h3>
+				<p>${data.content}</p>
+			
+
+    </div>
+    `;
+    console.log(animeQuote)
+    outPut.appendChild(animeQuote);
+   
+}
+
+ btn.addEventListener("click",  getQuote()) 
+})
+
+
+// document.addEventListener("DOMContentLoaded",result(getQuote())) 
